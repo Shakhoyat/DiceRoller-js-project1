13d926db36ef31ebe8bd52602dde860145645745
Md.Shakhoyat Rahman Shujon <shakoyatsujon@gmail.com>
2025-03-13 17:15:05 +0600

    its working fine

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 //DICE ROLLER PROGRAM
 
-function diceRoller() {
+function rolldice() {
   const numOfDice = document.getElementById("numOfDice").value;
   const diceResult = document.getElementById("diceResult").value;
   const diceImages = document.getElementById("diceImages").value;
+
+  const values = [];
+  const images = [];
+
+  for (let i = 0; i < numOfDice; i++) {
+    const value = Math.floor(Math.random() * 6) + 1;
+    console.log(value);
+    // values.push(value);
+    // images.push(`images/dice${value}.png`);
+  }
   // Generate a random number between 1 and 6
-  let randomNumber = Math.floor(Math.random() * 6) + 1;
-  return randomNumber;
+
+  // return randomNumber;
 }
